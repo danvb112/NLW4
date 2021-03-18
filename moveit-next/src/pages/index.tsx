46e5a30cb22b215dd865c7652114d2f219cc68ed
danvb112 <danielbonasser@gmail.com>
2021-03-18 11:26:36 -0300

    Add Countdown component

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,18 +1,24 @@
 import CompletedChallenges from "../components/CompletedChallenges";
+import Head from 'next/head'
 import ExperienceBar from "../components/ExperienceBar";
 import Profile from "../components/Profile";
+import Countdown from '../components/Countdown'
 
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
 
